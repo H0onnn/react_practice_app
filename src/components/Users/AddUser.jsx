13e936ec79469372549869e8678aacdb9f01c6ed
H0onnn <dukei2017@naver.com>
2023-05-23 23:40:32 +0900

    state를 활용한 userData 추가 기능

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,20 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 function AddUser(props) {
+  const [enteredUser, setEnteredUser] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+
+    if (enteredUser.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    props.onAddUser(enteredUser, enteredAge);
+
+    setEnteredUser("");
+    setEnteredAge("");
+  };
+
+  const enteredUserHandler = (e) => {
+    setEnteredUser(e.target.value);
+  };
+
+  const enteredAgeHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUser}
+          onChange={enteredUserHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={enteredAgeHandler}
+        />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
